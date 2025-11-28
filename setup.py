e1f1bc3a8f2e3427cff4d0dd9ae8ab479ba47a2e
mrunalshende4115 <74857442+mrunalshende4115@users.noreply.github.com>
2025-11-28 20:41:06 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
-
+# Setup Configuration for packaging and distributing
 setup(
-    name='quicktrack4115',
+    name='quicktrack4115', #Library name
     version='0.1.0',
     packages=find_packages(),
     author='Mrunal',
